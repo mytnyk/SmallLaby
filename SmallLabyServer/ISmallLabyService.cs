44f61b01b64665f37375c5c82ec6d40865a49d3c
Oleg <oleg.mytnyk@gmail.com>
2016-09-28 13:00:26 +0300

    Add project files.

---
+++ SmallLabyServer/ISmallLabyService.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+
+namespace SmallLabyServer
+{
+    [ServiceContract]
+    interface IPlayer
+    {
+        void Move();
+    }
+
+    [ServiceContract]
+    interface ISmallLaby
+    {
+        [OperationContract]
+        IPlayer AddPlayer(string name);
+        [OperationContract]
+        void RemovePlayer(IPlayer player);
+    }
+}
